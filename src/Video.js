baa0314725c34f7c242214d41a028902dbef90fb
ErikPeni <quyendung16@gmail.com>
2021-12-28 01:20:45 +0700

    v0.0.1

---
+++ src/Video.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Video.module.css';
+
+function Video() {
+	return (
+		<div className={styles.video}>
+			<h1>This is a video</h1>
+		</div>
+	);
+}
+
+export default Video;
